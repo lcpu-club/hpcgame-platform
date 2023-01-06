f963e2784951bc35bbf9367221962ba0f2145a5c
thezzisu <thezzisu@gmail.com>
2023-01-06 13:45:17 +0000

    feat: replace starry-night with lowlight

--- packages/ui/src/utils/md.ts
+++ packages/ui/src/utils/md.ts
@@ -1,59 +1,11 @@
-import { createStarryNight, common } from '@wooorm/starry-night'
-import { visit } from 'unist-util-visit'
-import { toString } from 'hast-util-to-string'
-import { unified, type Transformer } from 'unified'
-import type { Root } from 'hast'
+import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkMath from 'remark-math'
 import remarkGfm from 'remark-gfm'
 import remarkRehype from 'remark-rehype'
 import rehypeKatex from 'rehype-katex'
 import rehypeStringify from 'rehype-stringify'
-
-const starryNight = await createStarryNight(common)
-
-export function rehypeStarryNight(): Transformer<Root, Root> {
-  const prefix = 'language-'
-  return function (tree) {
-    visit(tree, 'element', function (node, index, parent) {
-      if (!parent || index === null || node.tagName !== 'pre') {
-        return
-      }
-      const head = node.children[0]
-      if (
-        !head ||
-        head.type !== 'element' ||
-        head.tagName !== 'code' ||
-        !head.properties
-      ) {
-        return
-      }
-      const classes = head.properties.className
-      if (!Array.isArray(classes)) return
-      const language = classes.find(
-        (d) => typeof d === 'string' && d.startsWith(prefix)
-      )
-      if (typeof language !== 'string') return
-      const scope = starryNight.flagToScope(language.slice(prefix.length))
-      if (!scope) return
-      const fragment = starryNight.highlight(toString(head), scope)
-      const children = /** @type {Array<ElementContent>} */ fragment.children
-      parent.children.splice(index, 1, {
-        type: 'element',
-        tagName: 'div',
-        properties: {
-          className: [
-            'highlight',
-            'highlight-' + scope.replace(/^source\./, '').replace(/\./g, '-')
-          ]
-        },
-        children: [
-          { type: 'element', tagName: 'pre', properties: {}, children }
-        ]
-      })
-    })
-  }
-}
+import rehypeHighlight from 'rehype-highlight'
 
 export function render(source: string): string {
   const file = unified()
@@ -61,7 +13,7 @@ export function render(source: string): string {
     .use(remarkMath)
     .use(remarkGfm)
     .use(remarkRehype, { allowDangerousHtml: true })
-    .use(rehypeStarryNight)
+    .use(rehypeHighlight)
     .use(rehypeKatex)
     .use(rehypeStringify, { allowDangerousHtml: true })
     .processSync(source)
--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -11,9 +11,6 @@ process.env.VITE_BUILD_TIME = new Date().toISOString()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    target: 'esnext'
-  },
   plugins: [vue(), windiCSS()],
   resolve: {
     alias: {
