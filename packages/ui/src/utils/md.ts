f143ce656e0247e85207bb5c42ab315a9c92b731
thezzisu <thezzisu@gmail.com>
2023-01-06 08:39:03 +0000

    feat: use unified for markdown processing

--- packages/ui/src/utils/md.ts
+++ packages/ui/src/utils/md.ts
@@ -1,37 +1,70 @@
-import md from 'markdown-it'
-import mk from 'markdown-it-katex'
 import { createStarryNight, common } from '@wooorm/starry-night'
-import { toHtml } from 'hast-util-to-html'
+import { visit } from 'unist-util-visit'
+import { toString } from 'hast-util-to-string'
+import { unified, type Transformer } from 'unified'
+import type { Root } from 'hast'
+import remarkParse from 'remark-parse'
+import remarkMath from 'remark-math'
+import remarkGfm from 'remark-gfm'
+import remarkRehype from 'remark-rehype'
+import rehypeKatex from 'rehype-katex'
+import rehypeStringify from 'rehype-stringify'
 
 const starryNight = await createStarryNight(common)
 
-const instance = md({
-  html: true,
-  linkify: true,
-  typographer: true,
-  highlight(value: string, lang: string) {
-    const scope = starryNight.flagToScope(lang)
-
-    return toHtml({
-      type: 'element',
-      tagName: 'pre',
-      properties: {
-        className: scope
-          ? [
-              'highlight',
-              'highlight-' + scope.replace(/^source\./, '').replace(/\./g, '-')
-            ]
-          : undefined
-      },
-      children: scope
-        ? starryNight.highlight(value, scope).children
-        : [{ type: 'text', value }]
+export function rehypeStarryNight(): Transformer<Root, Root> {
+  const prefix = 'language-'
+  return function (tree) {
+    visit(tree, 'element', function (node, index, parent) {
+      if (!parent || index === null || node.tagName !== 'pre') {
+        return
+      }
+      const head = node.children[0]
+      if (
+        !head ||
+        head.type !== 'element' ||
+        head.tagName !== 'code' ||
+        !head.properties
+      ) {
+        return
+      }
+      const classes = head.properties.className
+      if (!Array.isArray(classes)) return
+      const language = classes.find(
+        (d) => typeof d === 'string' && d.startsWith(prefix)
+      )
+      if (typeof language !== 'string') return
+      const scope = starryNight.flagToScope(language.slice(prefix.length))
+      if (!scope) return
+      const fragment = starryNight.highlight(toString(head), scope)
+      const children = /** @type {Array<ElementContent>} */ fragment.children
+      parent.children.splice(index, 1, {
+        type: 'element',
+        tagName: 'div',
+        properties: {
+          className: [
+            'highlight',
+            'highlight-' + scope.replace(/^source\./, '').replace(/\./g, '-')
+          ]
+        },
+        children: [
+          { type: 'element', tagName: 'pre', properties: {}, children }
+        ]
+      })
     })
   }
-})
-
-instance.use(mk)
+}
 
 export function render(source: string): string {
-  return instance.render(source)
+  const file = unified()
+    .use(remarkParse)
+    .use(remarkMath)
+    .use(remarkGfm)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeStarryNight)
+    .use(rehypeKatex)
+    .use(rehypeStringify, { allowDangerousHtml: true })
+    .processSync(source)
+
+  return String(file)
 }
