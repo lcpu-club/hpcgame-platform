a283fc6148d021811ecabec4ff275cbf9c58946a
thezzisu <thezzisu@gmail.com>
2023-01-06 14:06:49 +0000

    fix: SharedWorker not available on mobile

--- packages/ui/src/workers/index.ts
+++ packages/ui/src/workers/index.ts
@@ -2,14 +2,19 @@ import { authToken } from '@/api'
 import MessageWorker from './message?sharedworker'
 
 export function startMessageWorker() {
-  const worker = new MessageWorker()
-  worker.port.start()
-  worker.port.postMessage({
-    type: 'start',
-    payload: {
-      since:
-        parseInt(localStorage.getItem('messageTimestamp') ?? '0') || Date.now(),
-      token: authToken.value
-    }
-  })
+  try {
+    const worker = new MessageWorker()
+    worker.port.start()
+    worker.port.postMessage({
+      type: 'start',
+      payload: {
+        since:
+          parseInt(localStorage.getItem('messageTimestamp') ?? '0') ||
+          Date.now(),
+        token: authToken.value
+      }
+    })
+  } catch (err) {
+    console.log(err)
+  }
 }
