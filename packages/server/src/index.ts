05970ae38059988f7bb31803b122120bd5e49dc6
thezzisu <thezzisu@gmail.com>
2023-01-08 22:45:05 +0800

    feat: support clustered workers

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,21 @@
+import cluster from 'cluster'
 import minimist from 'minimist'
 
 const argv = minimist(process.argv.slice(2))
-const service = argv.service ?? argv.s ?? 'main'
+const service = String(argv.service ?? argv.s ?? 'main')
+const workers = Number(argv.workers ?? argv.w ?? 1)
 
-await import(`./services/${service}/index.js`)
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} started`)
+  for (let i = 0; i < workers; i++) {
+    cluster.fork()
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(
+      `worker ${worker.process.pid} died with code ${code} and signal ${signal}`
+    )
+  })
+} else {
+  console.log(`Worker ${process.pid} started`)
+  await import(`./services/${service}/index.js`)
+}
