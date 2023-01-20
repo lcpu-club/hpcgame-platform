bd1ee444937209a54d573dbb9e379535e1e7420b
thezzisu <thezzisu@gmail.com>
2023-01-20 13:38:02 +0000

    chore: update iaaa logic

--- packages/ui/src/views/auth/IaaaAuth.vue
+++ packages/ui/src/views/auth/IaaaAuth.vue
@@ -1,28 +1,82 @@
 <template>
-  <div class="flex items-center justify-center p-4">
-    <div>正在重定向到北京大学统一身份认证服务</div>
-    <form
-      action="https://iaaa.pku.edu.cn/iaaa/oauth.jsp"
-      method="post"
-      style="display: none"
-      ref="form"
-    >
-      <input type="hidden" name="appID" :value="appId" />
-      <input type="hidden" name="appName" :value="appName" />
-      <input type="hidden" name="redirectUrl" :value="redirectUrl" />
-    </form>
+  <div class="p-6 w-full hg-content-h-full flex items-center justify-center">
+    <div class="shadow max-w-128">
+      <NCard title="IAAA登录" segmented>
+        <NSpace align="center" v-if="isLoading">
+          <NSpin />
+          <div class="pl-4" v-if="iaaaToken">正在处理登录请求，请稍候</div>
+          <div class="pl-4" v-else>正在重定向到北京大学统一身份认证服务</div>
+        </NSpace>
+        <ErrorAlert v-else-if="error" :error="error" />
+        <form
+          action="https://iaaa.pku.edu.cn/iaaa/oauth.jsp"
+          method="post"
+          style="display: none"
+          ref="form"
+        >
+          <input type="hidden" name="appID" :value="appId" />
+          <input type="hidden" name="appName" :value="appName" />
+          <input type="hidden" name="redirectUrl" :value="redirectUrl" />
+        </form>
+        <template #action>
+          登录即视为您已同意
+          <RouterLink to="/terms" class="text-blue-600">参赛须知</RouterLink>。
+        </template>
+      </NCard>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, nextTick, onBeforeMount } from 'vue'
+import { NCard, NSpace, NSpin } from 'naive-ui'
+import { useAsyncState } from '@vueuse/core'
+import { mainApi, userInfo } from '@/api'
+import ErrorAlert from '@/components/misc/ErrorAlert.vue'
+import { HandlerFetchError } from 'typeful-fetch'
+import { finalizeLogin } from '@/utils/sync'
+import { RouterLink } from 'vue-router'
 const form = ref<HTMLFormElement>(null as never)
 
 const appId = import.meta.env.VITE_IAAA_ID
 const appName = import.meta.env.VITE_IAAA_NAME
 const redirectUrl = import.meta.env.VITE_IAAA_REDIRECT_URL
+const iaaaToken = ref('')
+
+const { isLoading, error, execute } = useAsyncState(
+  async () => {
+    const token = iaaaToken.value
+    try {
+      const user = await mainApi.auth.iaaa.$post.body({ token }).fetch()
+      userInfo.value = user
+      nextTick(() => {
+        finalizeLogin()
+      })
+    } catch (err) {
+      if (err instanceof HandlerFetchError) {
+        const { message } = await err.response.json()
+        throw new Error(message)
+      }
+    }
+  },
+  null,
+  { immediate: false }
+)
+
+onBeforeMount(() => {
+  const token = localStorage.getItem('iaaaToken')
+  if (token) {
+    iaaaToken.value = token
+    localStorage.removeItem('iaaaToken')
+  }
+})
 
 onMounted(() => {
-  form.value.submit()
+  if (iaaaToken.value) {
+    execute()
+  } else {
+    localStorage.setItem('iaaaRedirect', location.href)
+    form.value.submit()
+  }
 })
 </script>
--- packages/ui/src/views/auth/IaaaCallback.vue
+++ packages/ui/src/views/auth/IaaaCallback.vue
@@ -2,44 +2,18 @@
   <div class="p-6 w-full hg-content-h-full flex items-center justify-center">
     <div class="shadow max-w-128">
       <NCard title="IAAA登录" segmented>
-        <NSpace align="center" v-if="isLoading">
-          <NSpin />
-          <div class="pl-4">正在处理登录请求，请稍候</div>
-        </NSpace>
-        <ErrorAlert v-else-if="error" :error="error" />
-        <template #action>
-          登录即视为您已同意
-          <RouterLink to="/terms" class="text-blue-600">参赛须知</RouterLink>。
-        </template>
+        <div>正在解析IAAA响应，请稍候……</div>
       </NCard>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { NCard, NSpace, NSpin } from 'naive-ui'
-import { useAsyncState } from '@vueuse/core'
-import { mainApi, userInfo } from '@/api'
-import ErrorAlert from '@/components/misc/ErrorAlert.vue'
-import { HandlerFetchError } from 'typeful-fetch'
-import { finalizeLogin } from '@/utils/sync'
-import { nextTick } from 'vue'
-import { RouterLink } from 'vue-router'
+import { onMounted } from 'vue'
 
-const { isLoading, error } = useAsyncState(async () => {
+onMounted(() => {
   const token = new URLSearchParams(window.location.search).get('token')
-  if (!token) throw new Error('非法访问')
-  try {
-    const user = await mainApi.auth.iaaa.$post.body({ token }).fetch()
-    userInfo.value = user
-    nextTick(() => {
-      finalizeLogin()
-    })
-  } catch (err) {
-    if (err instanceof HandlerFetchError) {
-      const { message } = await err.response.json()
-      throw new Error(message)
-    }
-  }
-}, null)
+  token && localStorage.setItem('iaaaToken', token)
+  window.location.href = localStorage.getItem('iaaaRedirect') || '/'
+})
 </script>
